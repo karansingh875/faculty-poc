9d52e43007d55031f72e3e25eda769d8ba9cf55e
Karan Singh <karan.singh@digiqt.com>
2024-11-27 14:23:31 +0530

    Added loader

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,33 @@ function App() {
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
   const getLocation = () => {
+    setLoading(true);
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           setLatitude(position.coords.latitude);
           setLongitude(position.coords.longitude);
+          setLoading(false);
         },
         (error) => {
           setError(error.message);
+          setLoading(false);
         },
         {
           enableHighAccuracy: true,
-          timeout: 5000,
+          timeout: 15000,
           maximumAge: 0,
         }
       );
     } else {
       setError("Geolocation is not supported by this browser.");
+      setLoading(false);
     }
   };
   return (
-    <>
+    loading ? <h1>please wait while we are fetching the location</h1> : <>
       <button onClick={getLocation}>Show your location</button>
 
       <h3>
